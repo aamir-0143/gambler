934b7bd0707783ce5b5fc89cd4f2750121ace268
Md aamir reza <aamirreza156@gamil.com>
2021-12-23 10:32:06 +0530

    [Aamir] Add : Added this programs

---
+++ gambler/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gambler
+{
+    public class Class1
+    {
+       public void start()
+       { 
+        
+             int stake = 100;
+           int bet = 1;
+            Console.WriteLine("Total money : + money ");
+
+
+
+        }
+
+
+    }
+
+}
---
+++ gambler/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace gambler
+{
+    class Program
+    {
+        static void Main(string[] agrs)
+        {
+
+            Console.WriteLine("Total money  ");
+
+        }
+    }
+}
